5954eabc8bde3696533dac63459640c552de132b
Mateusz Judka <mrjudka@gmail.coM>
2023-07-14 23:35:23 +0100

    Adding API C# Work

---
+++ ZamApi/ZamApi/Models/GameDeal.cs
@@ -0,0 +1,23 @@
+﻿namespace ZamApi.Models
+// name, image, original price, sale price and a clickable link to the sale
+//https://www.cheapshark.com/api/1.0/deals
+//https://apidocs.cheapshark.com/
+{
+    public class GameDeal
+    {
+        public string title { get; set; }
+        public string gameID { get; set; }
+        public string thumb { get; set; }
+        
+        public string dealID { get; set; }
+
+        public string salePrice { get; set; }
+        public string normalPrice { get; set; }
+
+        public string savings { get; set; }
+        public string metacriticScore { get; set; }
+        public string steamRatingPercent { get; set; }
+        public string dealRating { get; set; }
+
+    }
+}
---
+++ ZamApi/ZamApi/Models/GameDealController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using System.Text.Json;
+
+namespace ZamApi.Models
+{
+    public class GameDealController : Controller
+    {
+
+        [Route("api/getgame/{id}")]
+        public async Task<string> getGame(string id)
+        {
+           if (id == "1")
+            {
+                GameDeal gameDeal = new GameDeal()
+                {
+
+                    title = "FirstGameDeal",
+                    salePrice = "11",
+                    normalPrice = "24",
+
+
+                };
+                return JsonSerializer.Serialize(gameDeal);
+ 
+            }
+            else
+            {
+                GameDeal gameDeal = new GameDeal()
+                {
+
+                    title = "TwoGameDeal",
+                    salePrice = "21",
+                    normalPrice = "32",
+
+
+                };
+                return JsonSerializer.Serialize(gameDeal);
+
+            }
+
+        }
+    }
+}
